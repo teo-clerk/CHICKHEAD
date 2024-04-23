849fab6b90d4f153f91405739a8babcaa0a7869f
Marc Pérez Fusco <07mapefu@gmail.com>
2024-04-23 14:02:23 +0200

    ScriptLiniaPredictoria

--- Scripts/Line2D.gd
+++ Scripts/Line2D.gd
@@ -1,11 +1,24 @@
 extends Line2D
 
-
+var posi :Vector2
+var posf :Vector2
+var vpos :Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed('Click'):
+		posi = get_viewport().get_mouse_position()
+	if Input.is_action_pressed('Click') and get_parent().is_on_floor():
+		posf = get_viewport().get_mouse_position()
+		vpos = posf - posi
+		clear_points()
+		add_point(Vector2(0, 0), -1)
+		add_point(vpos, -1)
+	else:
+		clear_points()
+	
