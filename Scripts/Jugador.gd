b222b2381fc1320c2a129589978e6fec2c1144ae
Marc Pérez Fusco <07mapefu@gmail.com>
2024-04-23 10:58:23 +0200

    ImplementacióMoviment

--- Scripts/Jugador.gd
+++ Scripts/Jugador.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 980
+var posi :Vector2
+var posf :Vector2
+var vpos :Vector2
+var posmax :int
 
 
 func _physics_process(delta):
@@ -13,6 +17,8 @@ func _physics_process(delta):
 	else:
 		jugador.animation = "default"
 	"""
+	if Input.is_action_just_pressed('Click'):
+		posi = get_viewport().get_mouse_position()
 	# Add the gravity.
 	if not is_on_floor():
 		#jugador.animation = "saltar"
@@ -20,15 +26,23 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_released('Click') and is_on_floor():
-		velocity = Global.VEL
-	else:
-		velocity.x = move_toward(velocity.x, 0, 20)
-		velocity.y = move_toward(velocity.y, 0, 20)
+		posf = get_viewport().get_mouse_position()
+		vpos = posf - posi
+		posmax = max(abs(vpos[0]), abs(vpos[1]))
+		if posmax > 125:
+			vpos[1] = vpos[1] * 500 / posmax
+			vpos[0] = vpos[0] * 500 / posmax
+		else:
+			vpos *= 4
+		velocity = vpos
+		print(vpos)
+	if is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, 25)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 
 	
-	var dir =  velocity.x < 0
-	jugador.flip_h = dir
+	#var dir =  velocity.x < 0
+	#jugador.flip_h = dir
 	move_and_slide()
