b9a20d68b49c26f3d310ef4bb68ae34a71771ff7
FerrerJan <166695352+FerrerJan@users.noreply.github.com>
2024-05-04 10:33:41 +0200

    rayo acabat + canvi de capa a les monedes

--- Scripts/rayo.gd
+++ Scripts/rayo.gd
@@ -1,6 +1,6 @@
 extends Area2D
-
-
+var rotacio = 1
+var animacio = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,3 +13,17 @@ func _process(delta):
 
 func _on_timer_timeout():
 	$Timer.start()
+	if rotacio == 1:
+		rotacio = 0
+		$AnimatedSprite2D.play('normal')
+		$CollisionShape2D.disabled = true
+		
+	else:
+		rotacio = 1
+		$CollisionShape2D.disabled = false
+		$AnimatedSprite2D.play('rayo')
+
+
+
+func _on_body_entered(body):
+	Global.dead = true
