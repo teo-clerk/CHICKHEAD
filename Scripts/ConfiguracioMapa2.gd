bbb751a33658f696930479c7f848fc8230e9ca60
Teo Clerici <clerici.teo5@gmail.com>
2024-05-12 15:49:29 +0200

    mapa finalizado

--- Scripts/ConfiguracioMapa2.gd
+++ Scripts/ConfiguracioMapa2.gd
@@ -4,5 +4,5 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.escena_anerior = "res://Scenes/mapa_2.tscn"# Replace with function body.
-	Global.claus_necessaries = 3
+	Global.claus_necessaries = 6
 
