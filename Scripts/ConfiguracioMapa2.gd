d3c4ba86847bb459c8c48222b6ef496811429f43
Teo Clerici <clerici.teo5@gmail.com>
2024-05-13 12:12:27 +0200

    Update ConfiguracioMapa2.gd

--- Scripts/ConfiguracioMapa2.gd
+++ Scripts/ConfiguracioMapa2.gd
@@ -5,4 +5,6 @@ extends Node
 func _ready():
 	Global.escena_anerior = "res://Scenes/mapa_2.tscn"# Replace with function body.
 	Global.claus_necessaries = 7
+	Global.claus = 0
+	Global.monedes = 0
 
