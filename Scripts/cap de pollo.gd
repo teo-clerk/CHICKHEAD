52889b8505caddf86402329a59400ea71cad06e5
Marc Pérez Fusco <07mapefu@gmail.com>
2024-05-17 12:09:06 +0200

    Cap acabat

--- Scripts/cap de pollo.gd
+++ Scripts/cap de pollo.gd
@@ -3,7 +3,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Global.cap:
+		queue_free()# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
