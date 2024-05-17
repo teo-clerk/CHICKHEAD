5272c4edbaecf6d52560695764668372cbd309a8
FerrerJan <166695352+FerrerJan@users.noreply.github.com>
2024-05-17 16:14:02 +0200

    credits

--- Node2D.gd
+++ Node2D.gd
@@ -12,4 +12,4 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/interfaz.tscn")
+	get_tree().change_scene_to_file("res://Scenes/opcions.tscn")
--- Scenes/opcions.gd
+++ Scenes/opcions.gd
@@ -18,8 +18,7 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	pass
-	 # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
 
 
 func _on_check_button_pressed():
