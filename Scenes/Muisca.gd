82fd52a949db76432493e8f45b2b244456193780
Marc Pérez Fusco <07mapefu@gmail.com>
2024-05-17 17:10:04 +0200

    MúsicaDesactivada

--- Scenes/Muisca.gd
+++ Scenes/Muisca.gd
@@ -1,13 +1,10 @@
 extends AudioStreamPlayer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	if !Global.music:
-		queue_free()
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Global.music and !playing:
+		playing = true
+	elif !Global.music:
+		playing = false
